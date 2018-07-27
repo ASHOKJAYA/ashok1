8d731e1f991571a7ef54029a21c52d844e4116bc
bapathu ashokreddy <41729604+ASHOKJAYA@users.noreply.github.com>
2018-07-27 11:14:13 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here\	
+		Scanner sc=new Scanner(System.in);
+		char ch = sc.next().charAt(0);
+		  if(ch == 'a' || ch == 'b' || ch == 'c' || ch == 'd' || ch == 'e' || ch == 'f' || ch == 'g' || ch == 'h' || ch == 'i'|| ch == 'j' || ch == 'k' || ch == 'l' || ch == 'm'|| ch == 'n' || ch == 'o' || ch == 'p' || ch == 'q'|| ch == 'r' || ch == 's' || ch == 't' || ch == 'u'|| ch == 'v' || ch == 'w' || ch == 'x' || ch == 'y' || ch == 'z')
+		 System.out.println("Alphabet");
+		 else
+			System.out.println("No");
+        
+	
+	}
+}
