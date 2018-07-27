f48dfe8e24d056e3dccff699fa622e1fc2361f42
bapathu ashokreddy <41729604+ASHOKJAYA@users.noreply.github.com>
2018-07-27 14:31:22 +0530

    Create plain.java

---
+++ plain.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int temp,r,sum=0;
+		temp=n;    
+		 while(n>0 && n<1000)
+		 {    
+			 r=n%10;  //getting remainder  
+			sum=(sum*10)+r;    
+			n=n/10;    
+		}    
+		 if(temp==sum)    
+			 System.out.println("Yes ");    
+		else    
+			 System.out.println("No");
+	}
+}
