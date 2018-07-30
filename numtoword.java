7394344902f6d3cd3a5b19a3967313fd54025ec0
bapathu ashokreddy <41729604+ASHOKJAYA@users.noreply.github.com>
2018-07-30 11:48:20 +0530

    Create numtoword.java

---
+++ numtoword.java
@@ -0,0 +1,44 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		    switch(n){  
+			   case 1:
+			   	System.out.println("One");  
+			  case 2:
+			  	System.out.println("Two");  
+			  case 3: 
+			  	System.out.println("Three");  
+			  case 4:
+			   	System.out.println("Four");  
+			  case 5:
+			  	System.out.println("Five");  
+			  case 6: 
+			  	System.out.println("Six");
+			  case 7:
+			   	System.out.println("Seven");  
+			  case 8:
+			  	System.out.println("Eight");  
+			  case 9: 
+			  	System.out.println("Nine"); 
+			  case 10: 
+			  	System.out.println("Ten");
+		    }
+
+
+
+		
+		
+
+	}
+}
