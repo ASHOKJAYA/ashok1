215b6b599fea0b3c257bf02f19516c74d2765eff
bapathu ashokreddy <41729604+ASHOKJAYA@users.noreply.github.com>
2018-07-31 14:46:12 +0530

    Create java88.java

---
+++ java88.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here     
+		int a, b, max, step, lcm = 0;
+      Scanner sc = new Scanner(System.in);
+      a = sc.nextInt();
+      b = sc.nextInt();
+
+      if(a > b){
+         max = step = a;
+      }
+      else{
+         max = step = b;
+      }
+
+      while(a!= 0) {
+         if(max % a == 0 && max % b == 0) {
+            lcm = max;
+            break;
+         }
+         max += step;
+      }
+      System.out.println(lcm);
+
+	}
+}
