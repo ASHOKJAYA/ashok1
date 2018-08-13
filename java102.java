9aff8cddf7ede6104d8eb68ff37763d5f9189754
bapathu ashokreddy <41729604+ASHOKJAYA@users.noreply.github.com>
2018-08-13 10:31:43 +0530

    Create java102.java

---
+++ java102.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+			do
+			{
+			if(num%2==0)
+			{
+				System.out.println(num/2);
+				num=num/2;
+			}else
+			{
+				System.out.println(num);
+			}
+			while(num<=1)
+
+			
+
+	}
+}
